d780b273dd44eeda243f08e70d037a79b602216c
hicso21 <tv21102000@gmail.com>
2024-02-13 16:10:00 -0300

    trying to fix limit #2

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const io = new Server(server, {
 // Middleware
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '200mb' }));
 app.use(express.json());
 app.use(
 	cors({
