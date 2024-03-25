03a174cd62a05a704265e8dd7d951de9616ab325
hicso21 <tv21102000@gmail.com>
2024-03-24 23:33:09 -0300

    fixes #5

--- server.js
+++ server.js
@@ -43,8 +43,11 @@ app.use(express.json());
 app.use(
 	cors({
 		origin: [
-			'http://localhost:5173/',
-			'https://runners-desktop.vercel.app/',
+			'http://localhost:5173',
+			'https://runners-desktop.vercel.app',
+			'https://delaf.host',
+			'https://desktop.delaf.host',
+			'exp://hg-cet.hicso.8081.exp.direct',
 		],
 		methods: ['OPTIONS', 'GET', 'PATCH', 'DELETE', 'POST', 'UPDATE', 'PUT'],
 	})
