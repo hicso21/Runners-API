fac2276706a64ac4d74c3b3342750638a415e195
hicso <hicso.dev@gmail.com>
2024-07-26 16:16:32 -0300

    Adding some endpoints to use webhooks of garmin #1

--- src/routes/v1/Garmin/index.js
+++ src/routes/v1/Garmin/index.js
@@ -7,9 +7,24 @@ const router = Router();
 router.get('/authorize/:db_id', GarminController.auth);
 router.get('/exchange_token', GarminController.exchange);
 router.post('/setStats', GarminController.setStats);
-router.get('/get_stats', async (req, res) => {
+router.get('/get_stats_activities', async (req, res) => {
 	const body = req.body;
-	await Test.create({ body: { ...body } });
+	await Test.create({ body: { ...body, webhook: 'activities' } });
+	res.end();
+});
+router.post('/get_stats_activities', async (req, res) => {
+	const body = req.body;
+	await Test.create({ body: { ...body, webhook: 'activities' } });
+	res.end();
+});
+router.get('/get_stats_activity_details', async (req, res) => {
+	const body = req.body;
+	await Test.create({ body: { ...body, webhook: 'activity_details' } });
+	res.end();
+});
+router.post('/get_stats_activity_details', async (req, res) => {
+	const body = req.body;
+	await Test.create({ body: { ...body, webhook: 'activity_details' } });
 	res.end();
 });
 
