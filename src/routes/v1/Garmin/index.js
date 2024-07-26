f481868d45b7fc76b9faba05c9089b23092eaf4a
hicso <hicso.dev@gmail.com>
2024-07-26 16:35:22 -0300

    Adding some endpoints to use webhooks of garmin #2

--- src/routes/v1/Garmin/index.js
+++ src/routes/v1/Garmin/index.js
@@ -8,24 +8,28 @@ router.get('/authorize/:db_id', GarminController.auth);
 router.get('/exchange_token', GarminController.exchange);
 router.post('/setStats', GarminController.setStats);
 router.get('/get_stats_activities', async (req, res) => {
-	const body = req.body;
-	await Test.create({ body: { ...body, webhook: 'activities' } });
-	res.end();
+    const body = req.body;
+    console.log('This is the GET of get_stats_activities', body);
+    await Test.create({ body: { ...body, webhook: 'activities' } });
+    res.end();
 });
 router.post('/get_stats_activities', async (req, res) => {
-	const body = req.body;
-	await Test.create({ body: { ...body, webhook: 'activities' } });
-	res.end();
+    const body = req.body;
+    console.log('This is the POST of get_stats_activities', body);
+    await Test.create({ body: { ...body, webhook: 'activities' } });
+    res.end();
 });
 router.get('/get_stats_activity_details', async (req, res) => {
-	const body = req.body;
-	await Test.create({ body: { ...body, webhook: 'activity_details' } });
-	res.end();
+    const body = req.body;
+    console.log('This is the GET of get_stats_activity_details', body);
+    await Test.create({ body: { ...body, webhook: 'activity_details' } });
+    res.end();
 });
 router.post('/get_stats_activity_details', async (req, res) => {
-	const body = req.body;
-	await Test.create({ body: { ...body, webhook: 'activity_details' } });
-	res.end();
+    const body = req.body;
+    console.log('This is the POST of get_stats_activity_details', body);
+    await Test.create({ body: { ...body, webhook: 'activity_details' } });
+    res.end();
 });
 
 export default router;
