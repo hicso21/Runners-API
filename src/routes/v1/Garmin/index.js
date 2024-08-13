5c319978f02a2139cbb80327a6ee2fbfd45b093f
hicso <hicso.dev@gmail.com>
2024-08-13 10:49:18 -0300

    Adding new routes on garmin webhooks

--- src/routes/v1/Garmin/index.js
+++ src/routes/v1/Garmin/index.js
@@ -17,11 +17,25 @@ router.post('/get_stats_activity_details', async (req, res) => {
     res.end();
 });
 
-// router.post('/get_stats_manually_updated_activities', async (req, res) => {
-//     const body = req.body;
-//     console.log('This is the POST of get_stats_manually_updated_activities', body);
-//     await Test.create({ body: { ...body, webhook: 'manually_updated_activities' } });
-//     res.end();
-// });
+router.post('/get_stats_activity_files', async (req, res) => {
+    const body = req.body;
+    console.log('This is the POST of get_stats_activity_files', body);
+    await Test.create({ body: { ...body, webhook: 'activity_files' } });
+    res.end();
+});
+
+router.post('/get_stats_move_iq', async (req, res) => {
+    const body = req.body;
+    console.log('This is the POST of get_stats_move_iq', body);
+    await Test.create({ body: { ...body, webhook: 'move_iq' } });
+    res.end();
+});
+
+router.post('/get_stats_manually_updated_activities', async (req, res) => {
+    const body = req.body;
+    console.log('This is the POST of get_stats_manually_updated_activities', body);
+    await Test.create({ body: { ...body, webhook: 'manually_updated_activities' } });
+    res.end();
+});
 
 export default router;
