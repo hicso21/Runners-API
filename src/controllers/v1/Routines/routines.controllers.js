bd66e1e17740be5a68e2016a894ab7ed4a65980a
hicso21 <hicso.dev@gmail.com>
2024-09-30 19:02:44 -0300

    Adding type on routines

--- src/controllers/v1/Routines/routines.controllers.js
+++ src/controllers/v1/Routines/routines.controllers.js
@@ -1,82 +1,81 @@
 import RoutinesServices from '../../../services/v1/Routines/routines.services.js';
 
 class RoutinesController {
-	static async getRoutines(req, res) {
-		try {
-			const routines = await RoutinesServices.getAllRoutines();
-			res.status(200).send(routines);
-		} catch (error) {
-			res.status(500).send({
-				error: true,
-				data: error,
-			});
-		}
-	}
+    static async getRoutines(req, res) {
+        try {
+            const routines = await RoutinesServices.getAllRoutines();
+            res.status(200).send(routines);
+        } catch (error) {
+            res.status(500).send({
+                error: true,
+                data: error,
+            });
+        }
+    }
 
-	static async newRoutine(req, res) {
-		try {
-			const { name } = req.body;
-			const exercises = [];
-			const newRoutine = await RoutinesServices.postRoutine(
-				name,
-				exercises
-			);
-			res.status(201).send(newRoutine);
-		} catch (error) {
-			res.status(500).send({
-				error: true,
-				data: error,
-			});
-		}
-	}
+    static async newRoutine(req, res) {
+        try {
+            const { name, type } = req.body;
+            const exercises = [];
+            const newRoutine = await RoutinesServices.postRoutine(
+                name,
+                exercises,
+                type
+            );
+            res.status(201).send(newRoutine);
+        } catch (error) {
+            res.status(500).send({
+                error: true,
+                data: error,
+            });
+        }
+    }
 
-	static async getOneRoutine(req, res) {
-		try {
-			const { id } = req.params;
-			const routine = await RoutinesServices.getOneRoutine(id);
-			res.status(200).send(routine);
-		} catch (error) {
-			res.status(500).send({
-				error: true,
-				data: error,
-			});
-		}
-	}
+    static async getOneRoutine(req, res) {
+        try {
+            const { id } = req.params;
+            const routine = await RoutinesServices.getOneRoutine(id);
+            res.status(200).send(routine);
+        } catch (error) {
+            res.status(500).send({
+                error: true,
+                data: error,
+            });
+        }
+    }
 
-	static async updateRoutine(req, res) {
-		try {
-			const { id } = req.params;
-			const routine = req.body;
-			const updatedRoutine = await RoutinesServices.updateRoutine(
-				id,
-				routine
-			);
-			res.status(200).send(updatedRoutine);
-		} catch (error) {
-			res.status(500).send({
-				error: true,
-				data: error,
-			});
-		}
-	}
+    static async updateRoutine(req, res) {
+        try {
+            const { id } = req.params;
+            const routine = req.body;
+            const updatedRoutine = await RoutinesServices.updateRoutine(
+                id,
+                routine
+            );
+            res.status(200).send(updatedRoutine);
+        } catch (error) {
+            res.status(500).send({
+                error: true,
+                data: error,
+            });
+        }
+    }
 
-	static async deleteRoutine(req, res) {
-		try {
-			const { id } = req.params;
-			await RoutinesServices.deleteRoutine(id);
-			res.status(200).send({
-				error: false,
-				data: 'Routine deleted correctly',
-			});
-		} catch (error) {
-			res.status(500).send({
-				error: true,
-				data: error,
-			});
-		}
-	}
-
-	
+    static async deleteRoutine(req, res) {
+        try {
+            const { id } = req.params;
+            await RoutinesServices.deleteRoutine(id);
+            res.status(200).send({
+                error: false,
+                data: 'Routine deleted correctly',
+            });
+        } catch (error) {
+            res.status(500).send({
+                error: true,
+                data: error,
+            });
+        }
+    }
 }
 
 export default RoutinesController;
--- src/services/v1/Routines/routines.services.js
+++ src/services/v1/Routines/routines.services.js
@@ -1,76 +1,77 @@
 import Routines from '../../../db/models/Routines.js';
 
 export default class RoutinesServices {
-	static async getAllRoutines() {
-		try {
-			const routines = await Routines.find({});
-			return routines;
-		} catch (error) {
-			return {
-				error: true,
-				data: error,
-			};
-		}
-	}
+    static async getAllRoutines() {
+        try {
+            const routines = await Routines.find({});
+            return routines;
+        } catch (error) {
+            return {
+                error: true,
+                data: error,
+            };
+        }
+    }
 
-	static async postRoutine(name, exercises) {
-		try {
-			const newRoutine = new Routines({
-				name,
-				exercises
-			});
-			await newRoutine.save();
-			return newRoutine;
-		} catch (error) {
-			return {
-				error: true,
-				data: error,
-			};
-		}
-	}
+    static async postRoutine(name, exercises, type) {
+        try {
+            const newRoutine = new Routines({
+                name,
+                exercises,
+                type,
+            });
+            await newRoutine.save();
+            return newRoutine;
+        } catch (error) {
+            return {
+                error: true,
+                data: error,
+            };
+        }
+    }
 
-	static async getOneRoutine(id) {
-		try {
-			const routine = await Routines.findById(id);
-			return routine;
-		} catch (error) {
-			return {
-				error: true,
-				data: error,
-			};
-		}
-	}
+    static async getOneRoutine(id) {
+        try {
+            const routine = await Routines.findById(id);
+            return routine;
+        } catch (error) {
+            return {
+                error: true,
+                data: error,
+            };
+        }
+    }
 
-	static async updateRoutine(id, routine) {
-		try {
-			const routineUpdated = await Routines.findByIdAndUpdate(
-				id,
-				routine,
-				{ new: true }
-			);
-			return routineUpdated;
-		} catch (error) {
-			return {
-				error: true,
-				data: error,
-			};
-		}
-	}
+    static async updateRoutine(id, routine) {
+        try {
+            const routineUpdated = await Routines.findByIdAndUpdate(
+                id,
+                routine,
+                { new: true }
+            );
+            return routineUpdated;
+        } catch (error) {
+            return {
+                error: true,
+                data: error,
+            };
+        }
+    }
 
-	static async deleteRoutine(id) {
-		try {
-			const res = await Routines.findByIdAndDelete(id);
-			return {
-				error: false,
-				data: res,
-				status: 200,
-			};
-		} catch (error) {
-			return {
-				error: true,
-				data: error,
-				status: 500,
-			};
-		}
-	}
+    static async deleteRoutine(id) {
+        try {
+            const res = await Routines.findByIdAndDelete(id);
+            return {
+                error: false,
+                data: res,
+                status: 200,
+            };
+        } catch (error) {
+            return {
+                error: true,
+                data: error,
+                status: 500,
+            };
+        }
+    }
 }
