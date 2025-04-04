f3ca6e77d48f86bae86a57aaa898a27b7d6fcb2c
hicso21 <hicso.dev@gmail.com>
2025-04-04 18:30:37 -0300

    First step adding audio on chats

--- src/db/models/GlobalChat.js
+++ src/db/models/GlobalChat.js
@@ -1,20 +1,21 @@
 import { Schema, model } from 'mongoose';
 
 const globalChatSchema = new Schema(
-	{
-		message: String,
-		from: String,
-		user_id: String,
-		profile_picture: String,
-		timestamp: Number,
-		on_response: Object,
-		createdAt: {
-			type: Date,
-			default: Date.now,
-			index: { expires: 129600 },
-		},
-	},
-	{ timestamps: true }
+    {
+        message: String,
+        from: String,
+        user_id: String,
+        profile_picture: String,
+        timestamp: Number,
+        is_audio: Boolean,
+        on_response: Object,
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            index: { expires: 129600 },
+        },
+    },
+    { timestamps: true }
 );
 
 const GlobalChats = model('GlobalChat', globalChatSchema);
--- src/db/models/UserChat.js
+++ src/db/models/UserChat.js
@@ -1,15 +1,16 @@
 import { Schema, model } from 'mongoose';
 
 const userChatSchema = new Schema(
-	{
-		message: String,
-		user_id: String,
-		name: String,
-		is_user: Boolean,
-		timestamp: Number,
-		createdAt: { type: Date, expires: 604800 },
-	},
-	{ timestamps: true }
+    {
+        message: String,
+        user_id: String,
+        name: String,
+        is_user: Boolean,
+        is_audio: Boolean,
+        timestamp: Number,
+        createdAt: { type: Date, expires: 604800 },
+    },
+    { timestamps: true }
 );
 
 const UserChats = model('UserChat', userChatSchema);
